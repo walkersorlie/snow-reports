e44add0aa3517f1d9c1baa8153672e666e3dc287
Walker Sorlie <walkersorlie@gmail.com>
2018-04-26 16:42:02 -0600

    Little tweaks for demo

--- resorts/management/commands/resort_scrapers.py
+++ resorts/management/commands/resort_scrapers.py
@@ -59,22 +59,16 @@ def alta():
 
     # with open('/home/walker/senior_proj/snow_site/reports/alta.html', 'w') as file:
     #     file.write(html)
-    #
+
     # with open("./reports/alta.html") as fp:
     #     soup = BeautifulSoup(fp, "lxml")
 
-    # snow_report = soup.find(id = "snow-report")
-    # tables = snow_report.find_all("table", limit = 3)
     tables = soup.find_all("table", class_ = "table table-weather", limit = 3)
     article_id = soup.find("article", id = "snow-report")
 
     # the Resort_Weather model fields
     time = article_id.find("p", class_ = "text-right")
-    # print time.get_text(strip=True)
-    # response.write((time.get_text(strip=True) + " "))
 
-    # last_updated_date = datetime.datetime.today()
-    # last_updated_date = localtime(now())
     last_updated_date = datetime.datetime.now()
     last_updated_time_field = time.get_text()
     twelve_hour_snow_field = ""
@@ -89,64 +83,39 @@ def alta():
     for count, table in enumerate(tables):
         entries = table.find_all("tr")
         if count == 0:  # snowfall
-            # There is a seperate div that has this info as well
             for specific, row in enumerate(entries):
                 if specific == 0:   # twelve_hour_snow
-                    # response.write(row)
-                    # twelve_hour_snow_field = str(row.find('td'))
                     twelve_hour_snow_field = row.find('td').get_text()
-                    # response.write((twelve_hour_snow_field + "<br><br>"))
                 elif specific == 1: # twenty4_hour_snow_field
-                    # response.write(row)
-                    # twenty4_hour_snow_field = str(row.find('td'))
                     twenty4_hour_snow_field = row.find('td').get_text()
-                    # response.write((twenty4_hour_snow_field + "<br><br>"))
-                elif specific == 3: # base_field
-                    # base_field = str(row.find('td'))
+                elif specific == 2: # base_field
                     base_field = row.find('td').get_text()
-                    # print base_field
-                    # response.write((base_field + "<br><br>"))
-                elif specific == 4: # total_snow_field
-                    # total_snow_field = str(row.find('td')) + " "
+                elif specific == 3: # total_snow_field
                     total_snow_field = row.find('td').get_text()
-                    # print total_snow_field
-                    # response.write((total_snow_field + "<br><br>"))
         elif count == 1:  # current_weather
             for specific, row in enumerate(entries):
                 if specific == 0:   # sky coverage
-                    # response.write(row)
                     table_rows = row.find_all('td')
-                    # current_weather_field = str(table_rows[1])
                     current_weather_field = table_rows[1].get_text()
-                    # response.write(current_weather)
                 elif specific == 1: # winds
                     table_rows = row.find_all('td')
-                    # current_weather_field = current_weather_field + str(table_rows[0]) + str(table_rows[1])
                     current_weather_field = current_weather_field +", " + table_rows[0].get_text() + " " +  table_rows[1].get_text() + " wind"
-                    # response.write((current_weather_field + "<br><br>"))
                 elif specific == 2: # temperature
-                    # current_temp_field = str(row.find('td'))
                     current_temp_field = row.find('td').get_text()
-                    # response.write((current_temp_field + "<br><br>"))
         elif count == 2:   # forecast
             for specific, row in enumerate(entries):
                 if specific == 0:   # sky cover
                     table_rows = row.find_all('td')
-                    # forecast_field = forecast_field + str(table_rows[1])
                     forecast_field = forecast_field + "Sky cover: " + table_rows[1].get_text() + "\n"
-                    # response.write(forecast_field)
                 elif specific == 1: # winds
                     table_rows = row.find_all('td')
                     forecast_field = forecast_field + "Winds: " + table_rows[0].get_text() + "/" +  table_rows[1].get_text() + "\n"
-                    # response.write(forecast_field)
                 elif specific == 2: # high temp range
                     table_rows = row.find('td')
                     forecast_field = forecast_field + "High temperature range: " + table_rows.get_text() + "\n"
-                    # response.write(forecast_field)
                 elif specific == 3: # expected snwofall
                     table_rows = row.find('td')
                     forecast_field = forecast_field + "Expected snowfall: " + table_rows.get_text()
-                    # response.write(forecast_field)
 
     # Resort_Weather model
     alta_weather = Resort_Weather(last_updated=last_updated_date, last_updated_time=last_updated_time_field, twelve_hour_snow=twelve_hour_snow_field, twenty4_hour_snow=twenty4_hour_snow_field, base=base_field, total_snow=total_snow_field, current_temp=current_temp_field, current_weather=current_weather_field, forecast=forecast_field, resort=foreign_key)
@@ -170,37 +139,19 @@ def snowbird():
 
     # the Resort_Weather model fields
     last_updated_date = datetime.datetime.today()
-    # last_updated_date = localtime(now())
-
     last_updated_time_field = time.get_text()
-    # response.write(last_updated_time_field + "<br><br>")
-
     twelve_hour_snow_field = tables[0].get_text()
-    # response.write(twelve_hour_snow_field + "<br><br>")
-
     twenty4_hour_snow_field = tables[1].get_text()
-    # response.write(twenty4_hour_snow_field + "<br><br>")
-
     base_field = tables[3].get_text()
-    # print base_field
-    # response.write(base_field + "<br><br>")
-
     total_snow_field = tables[4].get_text()
-    # response.write(total_snow_field + "<br><br>")
-
     current_temp_field = tables[6].get_text()
-    # response.write(current_temp_field + "<br><br>")
-
     current_weather_field = tables[8].get_text()
-    # response.write(current_weather_field + "<br><br>")
-
     forecast_field = ""
     foreign_key = Resort.objects.get(pk=2)
 
     for count, table in enumerate(forecast_tables):
         if count == 0:
             forecast_field = table.find('div', class_ = 'description').get_text()
-            # response.write(forecast_field)
 
     # Resort_Weather model
     snowbird_weather = Resort_Weather(last_updated=last_updated_date, last_updated_time=last_updated_time_field, twelve_hour_snow=twelve_hour_snow_field, twenty4_hour_snow=twenty4_hour_snow_field, base=base_field, total_snow=total_snow_field, current_temp=current_temp_field, current_weather=current_weather_field, forecast=forecast_field, resort=foreign_key)
@@ -223,29 +174,18 @@ def brighton():
     current_temp = current_weather_tables.find('div', class_= 'c_info quarter')
     current_snow = current_weather_tables.find('div', class_='c_snow quarter')
     snow_totals = current_weather_tables.find('div', class_='c_totals quarter')
-
     forecast = soup.find('div', class_='extended_forecast')
 
     last_updated_date = datetime.datetime.now()
-    # last_updated_date = localtime(now())
     last_updated_time_field = ""
     twelve_hour_snow_field = current_snow.find('h2').get_text()
     twenty4_hour_snow_field = ""
-    # base_field = snow_totals.find('h2').get_text()
     base_field = snow_totals.find('p').get_text()
     total_snow_field = ""
     current_temp_field = current_temp.find('h2').get_text()
     current_weather_field = current_temp.find('p').get_text()
-    # forecast_field = forecast.find('p').get_text()
     forecast_field = ""
     foreign_key = Resort.objects.get(pk=3)
-
-    # print twelve_hour_snow_field
-    # print base_field
-    # print total_snow_field
-    # print current_temp_field
-    # print current_weather_field
-    # print forecast_field
 
     # print forecast
     headers = forecast.find_all('h3', limit=3)
@@ -274,7 +214,6 @@ def solitude():
     weather_tables = soup.find('div', class_='snow-details')
     current_weather_div = weather_tables.find('div', class_='current')
     snow_totals = weather_tables.find('div', class_='snow-totals').find_all('div', class_='item')
-    #snow_totals = snow_totals_outer.find_all('div', class_='item')
     weather_details_outer = weather_tables.find('div', class_='weather-details')
     weather_details = weather_details_outer.find('ul').find_all('li')
 
@@ -282,7 +221,6 @@ def solitude():
     hilo = forecast.find('div', class_='hilo')
 
     last_updated_date = datetime.datetime.now()
-    # last_updated_date = localtime(now())
     last_updated_time_field = ""
     twelve_hour_snow_field = ""
     twenty4_hour_snow_field = ""
@@ -315,15 +253,6 @@ def solitude():
             temp = hilo.find('div', class_='low')
             forecast_field = forecast_field + "low: " + temp.find('span').get_text()
 
-    # print last_updated_time_field
-    # print twenty4_hour_snow_field
-    # print base_field
-    # print total_snow_field
-    # print current_temp_field
-    # print current_weather_field
-
     # Resort_Weather model
     solitude_weather = Resort_Weather(last_updated=last_updated_date, last_updated_time=last_updated_time_field, twelve_hour_snow=twelve_hour_snow_field, twenty4_hour_snow=twenty4_hour_snow_field, base=base_field, total_snow=total_snow_field, current_temp=current_temp_field, current_weather=current_weather_field, forecast=forecast_field, resort=foreign_key)
     solitude_weather.save()
-
-    # print "script ran at: " + datetime.datetime.now()
--- resorts/management/commands/uaac_scraper.py
+++ resorts/management/commands/uaac_scraper.py
@@ -22,28 +22,18 @@ class Command(BaseCommand):
         html = urlopen(htmlfile).read()
         soup = BeautifulSoup(html, "lxml")
 
-        with open('/home/walker/senior_proj/snow_site/reports/uaac.html', 'w') as file:
-          file.write(html)
+        # with open('/home/walker/senior_proj/snow_site/reports/uaac.html', 'w') as file:
+        #   file.write(html)
 
         # with open("home/walker/senior_proj/snow_site/reports/uaac.html") as fp:
         #     soup = BeautifulSoup(fp, "lxml")
 
         avalanche_problems = soup.find_all('div', class_ = 'clearbg avalanche-problem-row')
-        with open('/home/walker/senior_proj/snow_site/static/avi-problems.html', 'w') as file:
-            # file.write("%s" % soup.head)
-            # file.write("%s" % avalanche_problems)
-            file.write(str(soup.head))
-            file.write(str(avalanche_problems))
-
-        # for table in avalanche_problems:
-        #     problems_table = table.find('table', class_='avalanche-problem-table')
-        #     print problems_table
-        #     print
-        #     for count, td in enumerate(problems_table):
-        #         if count == 1:
-        #             print td
-        #             with open('/home/walker/senior_proj/snow_site/reports/avi-problems.html', 'w') as file:
-        #               file.write(td)
+        # with open('/home/walker/senior_proj/snow_site/static/avi-problems.html', 'w') as file:
+        #     # file.write("%s" % soup.head)
+        #     # file.write("%s" % avalanche_problems)
+        #     file.write(str(soup.head))
+        #     file.write(str(avalanche_problems))
 
         issued_by = soup.find(id = "subtitle-date-row")
         area_field = issued_by.find('th').get_text()
@@ -55,10 +45,6 @@ class Command(BaseCommand):
         current_conditions_field = soup.find(id = "current-conditions-row")
         current_conditions_field = current_conditions_field.find('p').get_text()
 
-        # recent_activity = soup.find(id = "recent-activity-row")
-        # response.write(recent_activity.find('span'))
-        # response.write(recent_activity.find('p'))
-
         weather_field = soup.find(id = "weather-row")
         weather_field = weather_field.find('p').get_text()
 
@@ -68,5 +54,3 @@ class Command(BaseCommand):
         # UAAC_Report model
         uaac_report = UAAC_Report(area=area_field, forcaster=forcaster_field, date=datetime.datetime.now(), advisory=advisory_field, current_conditions=current_conditions_field, recent_activity=recent_activity_field, weather=weather_field)
         uaac_report.save()
-
-        # self.stdout.write("It worked")
